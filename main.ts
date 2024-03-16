6aec5742654874d9f2af0b3bb180b5998787187b
Nico Baier <nico.baier@gmail.com>
2024-03-15 23:51:19 -0500

    added error handling

--- main.ts
+++ main.ts
@@ -2,18 +2,39 @@ const token = Deno.env.get("VALTOWN");
 const path = await Deno.realPath(Deno.args[0]);
 const file = new TextDecoder().decode(await Deno.readFile(path));
 
-const result = await fetch("https://api.val.town/v1/vals", {
-  method: "POST",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  },
+async function main() {
+  try {
+    if (!token) {
+      throw new Error("VALTOWN token not found");
+    }
+  } catch (e) {
+    console.error(e);
+    return;
+  }
 
-  body: JSON.stringify({
-    // TODO name: filename,
-    code: file,
-  }),
-});
+  try {
+    const result = await fetch("https://api.val.town/v1/vals", {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
 
-console.log(result.status);
+      body: JSON.stringify({
+        // TODO name: filename,
+        code: file,
+      }),
+    });
+
+    const { author, name } = await result.json();
+
+    console.log(
+      `Val created successfully! https://val.town/v/${author.username}/${name}`
+    );
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+main();
